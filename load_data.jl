9b63791ea7c246e555a1b25ba12dfb63019a1d48
jchen975 <47189952+jchen975@users.noreply.github.com>
2019-07-26 21:51:33 -0400

    Separated case_general_parallel into two parts
    load_data.jl performs 1) sampling from P, Q variations, 2) pf computation in parallel and 3) saving and loading existing data
    train.jl performs 1) training an MLP with provided dataset, 2) plotting results and 3) saving and loading existing model
    
    **case_general_parallel.jl** is no longer maintained from this point on. It's only meant to provide a single source file to run everything.

---
+++ load_data.jl
@@ -0,0 +1,227 @@
+using Distributed  # parallelize pf computations
+using Distributions
+using Distributions: Random
+using JLD2, FileIO
+
+# bus type
+const PQ_BUS = 1
+const PV_BUS = 2
+const REF_BUS = 3
+
+'''
+Only add processors if none has been added. The number of procs in addprocs()
+will be the ones doing parallel pf computation, so should be ~40 on clusters and
+5~7 on computers with 8 logical processors.
+When running on clusters, the third argument in bash file should be the number
+of worker processes to add. If none, then default to 5.
+'''
+if nprocs() == 1
+	if length(ARGS) == 3
+		addprocs(ARGS[3])
+	else
+		addprocs(5)
+end
+
+@everywhere using ParallelDataTransfer
+@everywhere using PowerModels, JuMP, Ipopt # powerl flow packages
+
+'''
+Parallel power flow computation
+
+After creating the P, Q matrices and parsing a matpower case file, compute
+DC and AC power flow in parallel as follows:
+	1. Pass P, Q, network_data to all worker processes
+	2. Using pmap() and its argument i, each worker process will first make a
+		deep copy of network_data dictionary, and then use the i-th column of
+		P, Q to update the pd, qd fields for all loads in network_data. Each
+		worker process will then use the run_dc_pf and run_ac_pf functions in
+		PowerModels to compute the DC and AC power flow results. Finally, each
+		worker returns the DC, AC power flow solutions and solution times as an
+		array of type Any, with the first two element being the solutions
+		dictionary and the last two being the solution times as Floats.
+	3. This will not cause race conditions as only the i-th column of P, Q are
+		accessed by each worker, and no worker process will access it twice.
+		No worker process writes to the shared P, Q matrices, and only writes
+		to the deep copied network_data, which is an overhead.
+
+Several notes:
+	1. @everywhere macro must be placed before all function definitions and
+		"using" statements if they are shared by workers
+	2. addprocs() must be called before all @everywhere instances
+'''
+@everywhere function compute_pf(i::Int64)
+	# println(i)  # uncomment this if you want to see which worker is executing what
+	network = deepcopy(ndata)  # we need to write to network dict so make copy
+	for j = 1:numPQ
+		network["load"][string(j)]["pd"] = P[j, i]
+		network["load"][string(j)]["qd"] = Q[j, i]
+	end
+	PowerModels.silence()
+	opt = Ipopt.Optimizer
+	dc_result = run_dc_pf(network, JuMP.with_optimizer(opt, print_level=0))
+	ac_result = run_ac_pf(network, JuMP.with_optimizer(opt, print_level=0))
+
+	ret = Array{Any}(undef, 4)
+	ret[1] = dc_result["solution"]
+	ret[2] = ac_result["solution"]
+	ret[3] = dc_result["solve_time"]
+	ret[4] = ac_result["solve_time"]
+	return ret
+end
+
+'''
+TODO: add variations for PV buses: P and Vm
+'''
+# load variations based on Zhen Dai's paper; NY parameters
+function get_PQ_variation(PD::Array{Float64}, baseMVA::Int64, N::Int64)
+	Random.seed!(521) # random seed for reproducible outputs
+	numPQ = length(PD)
+	P̄ = mean(PD) * baseMVA
+	α0 = 5.44130
+	α1 = 0.17459
+	α2 = 0.001673
+	σP = α0 + α1*sqrt(P̄) + α2*P̄
+
+	# Generate N (ΔPD, power factor(0.7~1.0 lagging)) independently Gaussian RVs
+	# for load bus PQ variations; Q = tan(acos(power factor)) * P
+	PD = rand(Normal(P̄, σP), N*numPQ) ./ baseMVA
+	pf = rand(TruncatedNormal(0.85, 0.1, 0.7, 1.0), N*numPQ)
+	QD = PD .* tan.(acos.(pf))
+
+	# reshape variations above from 1D vec to numPQ x N, and change all
+	# negative values to 0
+ 	PD = max.(reshape(PD, numPQ, :), 0)
+	QD = max.(reshape(QD, numPQ, :), 0)
+	return PD, QD
+end
+
+'''
+Perform dc and ac power flow computations or load existing data
+
+PowerModels parses matpower casefiles into a dictionary of dictionaries, where
+loads are viewed as explicit components that are separate from buses (they are
+two distinct dictionaries). So a bus can have none, 1, or many loads. The load
+objects have unique ids, which are independent from the bus ids. These load
+object ids are the keys in the load dictionary and the index value. The
+"load_bus" value tells you which bus id the load is connected to. Because buses
+can have no loads in PowerModels, when parsing Matpower data, loads with pd=0.0
+and qd=0.0 are filter out. loadToBusIdx is a 2D array to help connecting load
+and bus indices: first column is the load indices and the second the
+corresponding bus indices.
+
+ARGUMENTS:
+N = number of samples for the dataset
+case = "caseXXX", a string representing the name of the matpower casefile to
+	parse from.
+
+KEYWORD ARGUMENTS
+save_data: whether we save the computed pf results, default is false
+log: whether to print outputs to file, default is false
+
+Note that for precompilation, it is best to run the load_data first with a small
+	N once or twice, then the intended N with both save_data and log as true.
+	Also, @time macro should be placed before calling load_data if we want to
+	benchmark the performance, both time and memory usage. This will include the
+	pure pf time and other overhead.
+'''
+function load_data(N::Int64, case::String; save_data=false, log=false)
+	if isfile(string(case, "_pf_results_parallel.jld2")) == true
+		f = string(case, "_pf_results_parallel.jld2")
+		data = FileIO.load(f)["data"]
+		target = FileIO.load(f)["target"]
+		if log == true
+			log = open("output.log", "w")
+			println(log, "Total load data performance:")
+			close(log)
+		end
+		return data, target
+	end
+
+	# read matpower case file
+	network_data = parse_file(string(case, ".m"))
+	# bus = network_data["bus"]  # a dictionary containing bus info (NOT USED)
+	load = network_data["load"]
+	baseMVA = network_data["baseMVA"]
+	numPQ = length(load) # since load dictionary ignores 0 entries, its size <= length(bus)
+
+	# first col is loadIdx, second is its corresponding bus idx
+	loadToBusIdx = zeros(Int32, numPQ, 2)
+	PD = zeros(numPQ)
+	for i = 1:numPQ
+		PD[i] = load[string(i)]["pd"]
+		loadToBusIdx[i, 1] = 1  # ordered by load index
+		loadToBusIdx[i, 2] = load[string(i)]["load_bus"] # record corresponding bus number
+	end
+	PD, QD = get_PQ_variation(PD, baseMVA, N)
+	if save_data == true
+		save(string(case, "_pq_values_parallel.jld2"), "PD", PD, "QD", QD)
+	end
+
+	# generate input and label for NN
+	# make PD, QD, network_data and numPQ accessable by all workers
+	# each worker will only read from them, or first make deep copies then write
+	# to copies to avoid race condition
+	PowerModels.silence()
+	sendto(workers(), ndata = network_data, P = PD, Q = QD, numPQ = numPQ)  # so that every worker can access this
+
+	# run dc and ac pf in parallel
+	time = Base.time()
+	ret = pmap(compute_pf, 1:N)
+	pf_time = Base.time() - time
+
+	numFeature = Int32(4)  # pd, qd, vm_dc, va_dc, 1 PQ bus
+	numTarget = Int32(2)  # vm_ac, va_ac
+	xIncrement = numFeature - 1  # for indexing
+	yIncrement = numTarget - 1
+	data = zeros(Float32, numPQ*numFeature, N)  # pd, qd, vm_dc, va_dc ∀ PQ buses; samples are col vectors
+	target = zeros(Float32, numPQ*numTarget, N)
+	# reduce
+	time = Base.time()
+	dc_time, ac_time = .0, .0
+	for i = 1:N
+		x, y = Int32(1), Int32(1)
+		for k = 1:numPQ
+			# PD, QD access should be optimized here!
+			# ret[i][1], ret[i][2] = i-th solution ∀ i = 1, ..., N;
+			# 1 = dc solution, 2 = ac solution, 3 = dc solve time
+			data[x:x+xIncrement, i] = [
+							1.,
+							ret[i][1]["bus"][string(loadToBusIdx[k, 2])]["va"],
+							PD[k, i],
+							QD[k, i]]
+			target[y:y+yIncrement, i] = [
+							ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["vm"],
+							ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["va"]]
+			x += numFeature
+			y += numTarget
+		end
+		dc_time += ret[i][3]
+		ac_time += ret[i][4]
+	end
+	reduce_time = Base.time() - time
+
+	# currently dc_time and ac_time are both *actual* times, i.e. there are
+	# overlaps between workers due to parallel execution; the ratio however is
+	# true, so normalize it down with total parallel pf time
+	dc = (dc_time / (dc_time+ac_time)) * 100.0
+	ac = (ac_time / (dc_time+ac_time)) * 100.0
+
+	# DC and AC computation time; will be less than the @time macro in main()
+	# since that also has other overhead like network data dict accessing
+	# only write to output.log if we're saving data, i.e. not test runs
+	if save_data == true && log == true
+		log = open("output.log", "w")
+		println(log, "Number of workers: $(nprocs()-1)")
+		println(log, "Total power flow computation time: $(round(pf_time, digits=3)) seconds")
+		println(log, "  => dcpf: $(round(dc, digits=3)) %")
+		println(log, "  => acpf: $(round(ac, digits=3)) %")
+		println(log, "Extracting results time: $(round(reduce_time, digits=3)) seconds")
+		println("Total load data performance:")
+		close(log)
+	end
+	# save data and target to disk
+	if save_data == true
+		save(string(case, "_pf_results.jld2"), "data", data, "target", target)
+	end
+	return data, target
+end
---
+++ train.jl
@@ -0,0 +1,171 @@
+'''
+We assume there is CUDA hardware; if error(s) are reported, comment it out and
+" |> gpu" will be no-ops
+'''
+using CuArrays
+using Flux  # ML library
+using Flux.Optimise: ADAM
+using Plots
+# saving and loading files
+using JLD2, BSON
+using BSON: @save
+
+'''
+ARGUMENTS:
+case: matpower case file, a string with the format of "caseXXX"
+data, target: dataset and ground truth, must be of dimension (K, N) where
+		N = number of samples, K = number of features, as specified in Flux.jl
+		documentation
+K1: a positive integer, first hidden layer size
+
+KEYWORD ARGUMENTS:
+lr: learning rate, default = 1e-3
+epochs: default = 30
+batch_size: a positive integer default 64.
+K2: second hidden layer size, default DNE. In almost all cases having two hidden
+		layers is sufficient.
+retrain: if "true" and a trained model already exists in current directory, then
+		load the model, perform forward pass with data and target and exit.
+		default is "false".
+
+Saves trained model with checkpointing, as well as the loss and accuracy data
+in local directory
+'''
+function train_net(case::String, data::Array{AbstractFloat}, target::Array{AbstractFloat},
+				K1::Int64; lr::Float64=1e-3, epochs::Int64=30,
+				batch_size::Int64=64, K2::Int64=0, retrain=false)
+	# Float32 should decrease memory allocation demand and run much faster on
+	# non professional GPUs
+	if typeof(data) != Array{Float32, 2}
+		data = convert(Array{Float32}, data)
+	end
+	if typeof(target) != Array{Float32, 2}
+		target = convert(Array{Float32}, target)
+	end
+	f = string(case, "_model.bson")
+
+	# check if model is trained and does not need to be retrained
+	if isfile(f) == true && retrain == false
+		log = open("output.log", "w")
+		println(log, "Model already trained! Loading model for forward pass...")
+			start = Base.time()
+			BSON.@load string(filename, "_model.bson") model
+			model |> gpu
+			data |> gpu
+			predict = model(data)  # do stuff with this
+			elapsed = Base.time() - start
+		println(log, "Loading model and forward pass took $(round(elapsed, digits=3)) seconds.")
+		close(log)
+
+	# separate out train (70%), validation (15%) and test (15%) data
+	# don't move train set to gpu yet; may not have enough memory
+	N = size(data)[2]
+	trainSplit = round(Int32, N*0.7)
+	valSplit = round(Int32, N*0.85)
+	trainData = data[:, 1:trainSplit]
+	trainTarget = target[:, 1:trainSplit]
+	valData = data[:, trainSplit:valSplit] |> gpu
+	valTarget = target[:, trainSplit:valSplit] |> gpu
+	testData = data[:, valSplit:end] |> gpu
+	testTarget = target[:, valSplit:end] |> gpu
+
+	# network model: if K2 is nonzero, there are two hidden layers, otherwise 1
+	layer1 = Dense(size(data)[1], K1, relu)  # weight matrix 1
+	layer2 = Dense(K1, size(target)[1]) # weight matrix 2
+	model = Chain(layer1, layer2) |> gpu  # foward pass: ŷ = model(x)
+	nlayers = 1  # number of hidden layer(s)
+	if K2 != 0  # second hidden layer, optional
+		layer2 = Dense(K1, K2, relu)
+		layer3 = Dense(K2, size(target)[1])
+		model = Chain(layer1, layer2, layer3) |> gpu
+		nlayers = 2
+	end
+
+	# record loss/accuracy data for three sets
+	trainLoss, trainAcc, valLoss, valAcc = Float32[], Float32[], Float32[], Float32[]
+	epochTrainLoss, epochTrainAcc = 0.0, 0.0
+	batchData, batchTarget = undef, undef
+
+	# loss function and accuracy measure
+	loss(x, y) = Flux.mse(model(x), y)
+	accuracy(x, y) = 1 - abs.(mean(y - model(x)))
+
+	opt = ADAM(lr)  # ADAM optimizer
+	# opt = Momentum(lr)  # SGD with momentum
+
+	elapsedEpochs = 0
+	training_time = 0.0  # excludes early stop condition & checkpointing overhead
+
+	# train with mini batches; if batch_size = 0, train with full batch
+	randIdx = collect(1:1:size(trainData)[2])
+	if batch_size > 0
+		numBatches = round(Int, floor(size(trainData)[2] / batch_size - 1))
+	else
+		numBatches = 1
+		batch_size = size(trainData)[2] - 1
+
+	for epoch = 1:epochs
+		time = Base.time()
+		println("epoch: $epoch")
+		Random.shuffle!(randIdx) # to shuffle training set
+		i = 1  # Julia is 1 indexed
+		for j = 1:numBatches
+			# println(j)
+			batchData = trainData[:, randIdx[i:i+batch_size]] |> gpu
+			batchTarget = trainTarget[:, randIdx[i:i+batch_size]] |> gpu
+			Flux.train!(loss, Flux.params(model), [(batchData, batchTarget)], opt)
+			epochTrainLoss += Tracker.data(loss(batchData, batchTarget))
+			epochTrainAcc += Tracker.data(accuracy(batchData, batchTarget))
+			i += batch_size
+		end
+		push!(trainLoss, epochTrainLoss / numBatches)
+		push!(trainAcc, epochTrainAcc / numBatches)
+		push!(valLoss, Tracker.data(loss(valData, valTarget)))
+		push!(valAcc, Tracker.data(accuracy(valData, valTarget)))
+		epochTrainLoss, epochTrainAcc = 0.0, 0.0   # reset values
+		training_time += Base.time() - time
+		elapsedEpochs = epoch
+
+		# checkpoint if val acc increased; to load, change @save to @load
+		if epoch > 1 && valAcc[end] > valAcc[end-1]
+			model_checkpt = cpu(model)
+			@save string(case, "_ep", epoch, "_model.bson") model_checkpt  # to get weights, use Tracker.data()
+		end
+		# early exit condition
+		if trainAcc[end] > 0.99 && valAcc[end] > 0.99
+			break
+		end
+	end
+	testAcc = Tracker.data(accuracy(testData, testTarget))  # test set accuracy
+	log = open("output.log", "w")
+	println(log, "Finished training after $elapsedEpochs epochs and $(round(
+					training_time, digits=3)) seconds")
+	println(log, "Hyperparameters used: learning rate = $lr,
+				batch_size = $batch_size, number of hidden layers = $nlayers")
+	println(log, "Test set accuracy: $(round(testAcc*100, digits=3))%")
+	close(log)
+	model = cpu(model)
+	@save string(case, "_model.bson") model  # to get weights, use Tracker.data()
+	# save loss and accuracy data
+	save(string(case, "loss_acc_data.jld2"),
+		"trainLoss", trainLoss, "trainAcc", trainAcc, "valLoss", valLoss, "valAcc", valAcc)
+	plot_results(trainLoss, trainAcc, valLoss, valAcc, case)
+end
+
+'''
+Plots the loss and accuracy curves of training and validation sets, and saves
+the figures as PNGs in local directory.
+'''
+function plot_results(trainLoss::Array{Float32, 1}, trainAcc::Array{Float32, 1},
+					valLoss::Array{Float32, 1}, valAcc::Array{Float32, 1}, filename::String)
+	minAcc = min(minimum(trainAcc), minimum(valAcc))*.9  # for y axis limit
+	n = collect(1:1:length(trainLoss))  # horizontal axis
+	labels = ["Training", "Validation"]
+
+	plot(n, trainLoss, title="Loss", label=labels[1], xlabel="epoch", ylabel="loss")
+	plot!(n, valLoss, label=labels[2], xlabel="epoch", ylabel="loss")
+	png(string(filename, "_loss_plot"))
+	plot(n, trainAcc, title="Accuracy", label=labels[1], xlabel="epoch", ylabel="accuracy")
+	plot!(n, valAcc, label=labels[2], xlabel="epoch", ylabel="accuracy", legend=:right, ylims=(minAcc, 1))
+	png(string(filename, "_accuracy_plot"))
+end
