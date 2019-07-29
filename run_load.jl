ac9ef6f6ca181125e07427a14188bb9b73ba11fa
jchen975 <jeffreychen975@gmail.com>
2019-07-28 23:31:00 -0400

    Control flow restructure; added reload option

--- run_load.jl
+++ run_load.jl
@@ -4,32 +4,43 @@ will be the ones doing parallel pf computation, so should be ~40 on clusters and
 5~7 on computers with 8 logical processors.
 
 Running on command line (assuming load_data.jl is in current directory):
-`/path/to/julia run_load.jl <case name> <number of samples> <number of workers>`
+`/path/to/julia run_load.jl <case name> <number of samples> <number of workers>
+		<force generating new dataset Y/N>`
 """
 
 using Dates
 using Distributed
 
-if length(ARGS) == 3
-    case = ARGS[1]  # case name, is String
-    N = parse(Int64, ARGS[2])  # number of samples, is Int
+error = false
+case = ARGS[1]
+if length(ARGS) != 4
+	println("Incorrect number of arguments provided. Expected 4, received $(length(ARGS))")
+	error = true
+elseif isfile("load_data.jl") == false
+	println("load_data.jl not found in current directory. Exiting...")
+	error = true
+elseif isfile("$case.m") == false
+	println("$case.m not found in current directory. Exiting...")
+	error = true
+end
+
+if error == false
+	N = parse(Int64, ARGS[2])  # number of samples, is Int
 	nworker = parse(Int64, ARGS[3])  # number of worker processes, is Int
-    println("Running load_data for $case, $N samples, with $nworker workers")
-	if isfile("load_data.jl") == false
-		println("load_data.jl not found in current directory. Exiting...")
-		exit()
-	end
-	if isfile("$case.m") == false
-		println("$case.m not found in current directory. Exiting...")
-		exit()
-	end
+	reload = (ARGS[4]=="Y" || ARGS[4]=="y") ? true : false
+	println("Running load_data for $case, $N samples, with $nworker workers")
+
 	# only add worker processes and include load_data.jl if passes error checking
 	if nprocs() == 1
 		addprocs(nworker)
 	end
 	include("load_data.jl")
-	log = open("$(case)_pf_output.log", "a")  # record current time stamp
+	# record current time stamp at the start of log
+	log = open("$(case)_pf_output.log", "a")
 	println(log, now())
+	if reload == true
+		println(log, "Force generating new dataset even if one exists.")
+	end
 	close(log)
 
 	load_data(case, 10)  # precompilation run 1
@@ -37,8 +48,6 @@ if length(ARGS) == 3
 	load_data(case, 20)  # precompilation run 2
 	println("Warm up 2 successeful")
 	println("Finished warming up, starting data generation of $N samples with $nworker workers")
-	@time load_data(case, N, true, true, true)  # full set and save outputs to file
+	@time load_data(case, N, true, true, reload)  # full set and save outputs to file
 	println("Program finished. Exiting...")
-else
-	println("Incorrect number of arguments provided. Expected 3, received $(length(ARGS))")
 end
