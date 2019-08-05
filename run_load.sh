5b2b79249dbf809b44ba2ffeb1cf2c0ca545a01f
jchen975 <jeffreychen975@gmail.com>
2019-08-05 12:33:53 -0400

    Changed arguments to be shell variables
    TODO: case files loop in shell

--- run_load.sh
+++ run_load.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "------------------------------------------------------------------------" >> run_load.log
+# overwrite existing run_load.log or create a new one
+echo "------------------------------------------------------------------------" > run_load.log
 echo "************ Starting program: $(date) ************" >> run_load.log
 echo "------------------------------------------------------------------------" >> run_load.log
 
@@ -8,11 +9,16 @@ echo "------------------------------------------------------------------------" 
 # Running on command line (assuming load_data.jl is in current directory):
 # julia run_load.jl <case name> <number of samples> <number of workers> <force generating new dataset Y/N>
 
-julia run_load.jl case30 12000 40 Y >> run_load.log
-julia run_load.jl case118 12000 40 Y >> run_load.log
-julia run_load.jl case300 12000 40 Y >> run_load.log
-julia run_load.jl case2869 12000 40 Y >> run_load.log
-julia run_load.jl case13659 12000 40 Y >> run_load.log
+lscpu >> run_load.log  # print computer info to log file
+
+N=10000
+nprocs=30
+
+julia run_load.jl case30 $N $nprocs Y >> run_load.log
+julia run_load.jl case118 $N $nprocs Y >> run_load.log
+julia run_load.jl case300 $N $nprocs Y >> run_load.log
+julia run_load.jl case2869 $N $nprocs Y >> run_load.log
+julia run_load.jl case13659 $N $nprocs Y >> run_load.log
 
 echo "------------------------------------------------------------------------" >> run_load.log
 echo "************ Finished program: $(date) ************" >> run_load.log
