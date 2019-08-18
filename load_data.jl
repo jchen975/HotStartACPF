62bbdb3ccebe94db67d02be50eca76b1de7a0537
jchen975 <jeffreychen975@gmail.com>
2019-08-17 23:19:42 -0400

    Merged run_load.jl and load_data.jl
    - run_load.jl is now main() of load_data.jl. Note that `ARGS` is not available if running in REPL; arguments needs to be passed on command line
    - Solve base case before generating dataset
    - Normalize PD, QD to [0, 1] range
    - Reorganized dataset format; now is [P, Q, VM, VA] for data and [VM, VA] instead of 4 or 2 features per bus
    - Compute average acpf run time
    - Minor changes to output format
    
    TODO: add # iterations per acpf, and time per iteration

--- load_data.jl
+++ load_data.jl
@@ -1,14 +1,13 @@
 using Distributions, Random
-using Distributions: Random
-using JLD2, FileIO
-
-# bus type
-const PQ_BUS = 1
-const PV_BUS = 2
-const REF_BUS = 3
-
+using JLD2, FileIO, Dates
+using Distributed
 @everywhere using ParallelDataTransfer
 @everywhere using PowerModels, JuMP, Ipopt # powerl flow packages
+
+# bus type; PV, and REF not used yet
+const PQ_BUS = 1
+# const PV_BUS = 2
+# const REF_BUS = 3
 
 """
 	compute_pf(i::Int64)
@@ -52,7 +51,7 @@ Several notes:
 	ret[2] = ac_result["solution"]
 	ret[3] = dc_result["solve_time"]
 	ret[4] = ac_result["solve_time"]
-	# println("sample $i; ac: $(ret[4]); dc: $(ret[3])")
+	println("sample $i; ac: $(ret[4]); dc: $(ret[3])")
 	return ret
 end
 
@@ -61,19 +60,20 @@ Load demand variations based on Zhen Dai's paper; NY parameters
 
 TODO: add variations for PV buses: P and Vm
 """
-function get_PQ_variation(busPD::Array{Float64}, N::Int64)
+function get_PQ_variation(busPD::Array{Float64}, N::Int64, numPQ::Int64)
 	α0 = 5.44130
 	α1 = 0.17459
 	α2 = 0.001673
 	Random.seed!(521)
-	numPQ = length(busPD)
+
 	PD, QD = zeros(numPQ, N), zeros(numPQ, N)
-	pf = rand(TruncatedNormal(1, 0.05, 0.7, 1.0), numPQ)  # power factor distribution
+	pf = rand(TruncatedNormal(1, 0.05, 0.7, 1.0), N)  # power factor distribution
+
 	for i = 1:numPQ
 		perm = randperm(N)  # permutation index
 		σP = α0 + α1*sqrt(abs(busPD[i])) + α2*abs(busPD[i])  # even with negative load, std is the same
-		PD[i, :] = (rand(Normal(busPD[i], σP), N))[perm]
-	   	QD[i, :] = (PD[i, :] .* tan.(acos.(pf[perm])))[perm]
+		PD[i, :] = (rand(Normal(busPD[i], σP), N))
+	   	QD[i, :] = (PD[i, :] .* tan.(acos.(pf[perm])))  # permute power factor values
 		# if busPD[i] > 0  # only limit to postive values if og val is positive
 		# 	PD[i, :] = max.(PD[i, :], 0)
 		# 	QD[i, :] = max.(QD[i, :], 0)
@@ -121,16 +121,25 @@ function load_data(case::String, N::Int64, save_data::Bool=false,
 		# data = FileIO.load(f)["data"]
 		# target = FileIO.load(f)["target"]
 		if log == true
-			runlog = open("$(case)_output_pf.log", "a")
+			runlog = open("$(case)_pf_output_w$(nprocs()-1).log", "a")
 			println(runlog, "Dataset already exists in current directory.")
 			# println("Total load data performance:")
 			close(runlog)
 		end
 		return nothing
 	end
+	time = Base.time()
 	PowerModels.silence()
-	# read matpower case file
+
+	# read matpower case file, solve the base case acpf and overwrite existing
 	network_data = parse_file("$case.m")
+	sol = run_ac_pf(network_data, with_optimizer(Ipopt.Optimizer,
+					print_level=0, tol=1e-6, max_iter=150))["solution"]["bus"]
+	for b in sol  # b[1] = bus index Int, b[2] = solved values as dict
+		network_data["bus"][string(b[1])]["vm"] = b[2]["vm"]
+		network_data["bus"][string(b[1])]["va"] = b[2]["va"]
+	end
+
 	load = network_data["load"]
 	baseMVA = float(network_data["baseMVA"])  # cast as Float64
 	numPQ = length(load) # load dictionary ignores 0 entries ∴ numPQ <= length(bus)
@@ -146,7 +155,7 @@ function load_data(case::String, N::Int64, save_data::Bool=false,
 
 	# calculate PD, QD variations if haven't done so already
 	if isfile("$(case)_pq_values.jld2") == false
-		PD, QD = get_PQ_variation(busPD, N)
+		PD, QD = get_PQ_variation(busPD, N, numPQ)
 		PD = PD ./ baseMVA  # back to per unit
 		QD = QD ./ baseMVA
 		if save_data == true
@@ -158,8 +167,8 @@ function load_data(case::String, N::Int64, save_data::Bool=false,
 	end
 
 	if log == true
-		runlog = open("$(case)_output_pf.log", "a")
-		println(runlog, "Starting parallel dcpf and acpf at $(now()).")
+		runlog = open("$(case)_pf_output_w$(nprocs()-1).log", "a")
+		println(runlog, "Starting parallel dcpf and acpf at $(now())...")
 		close(runlog)
 	end
 
@@ -168,41 +177,36 @@ function load_data(case::String, N::Int64, save_data::Bool=false,
 	# each worker will only read from them, or first make deep copies then write
 	# to copies to avoid race condition
 	sendto(workers(), ndata = network_data, P = PD, Q = QD, numPQ = numPQ)  # so that every worker can access this
+	setup_time = Base.time() - time  # setting up overhead
 
 	# run dc and ac pf in parallel
 	time = Base.time()
 	ret = pmap(compute_pf, 1:N)
 	pf_time = Base.time() - time
 
-	numFeature = Int32(4)  # pd, qd, vm_dc, va_dc, 1 PQ bus
-	numTarget = Int32(2)  # vm_ac, va_ac
-	xIncrement = numFeature - 1  # for indexing
-	yIncrement = numTarget - 1
-	data = zeros(Float32, numPQ*numFeature, N)  # pd, qd, vm_dc, va_dc ∀ PQ buses; samples are col vectors
-	target = zeros(Float32, numPQ*numTarget, N)
+	# before inserting PD, QD into data matrix, normalize it to 0~1
+	maxPD, maxQD = maximum(PD), maximum(QD)
+	minPD, minQD = minimum(PD), minimum(QD)
+	PD = (PD .- minPD) ./ (maxPD - minPD)
+	QD = (QD .- minQD) ./ (maxQD - minQD)
+
+	VM_dc, VA_dc = ones(Float32, numPQ, N), zeros(Float32, numPQ, N)
+	VM_ac, VA_ac = zeros(Float32, numPQ, N), zeros(Float32, numPQ, N)
+
 	# reduce
 	time = Base.time()
 	dc_time, ac_time = .0, .0
 	for i = 1:N
-		x, y = Int32(1), Int32(1)
 		for k = 1:numPQ
-			# PD, QD access should be optimized here!
-			# ret[i][1], ret[i][2] = i-th solution ∀ i = 1, ..., N;
-			# 1 = dc solution, 2 = ac solution, 3 = dc solve time
-			data[x:x+xIncrement, i] = [
-							1.,
-							ret[i][1]["bus"][string(loadToBusIdx[k, 2])]["va"],
-							PD[k, i],
-							QD[k, i]]
-			target[y:y+yIncrement, i] = [
-							ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["vm"],
-							ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["va"]]
-			x += numFeature
-			y += numTarget
+			VM_ac[k, i] = ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["vm"]
+			VA_ac[k, i] = ret[i][2]["bus"][string(loadToBusIdx[k, 2])]["va"]
+			VA_dc[k, i] = ret[i][1]["bus"][string(loadToBusIdx[k, 2])]["va"]
 		end
 		dc_time += ret[i][3]
 		ac_time += ret[i][4]
 	end
+	data = vcat(PD, QD, VM_dc, VA_dc)
+	target = vcat(VM_ac, VA_ac)
 	reduce_time = Base.time() - time
 
 	# currently dc_time and ac_time are both *actual* times, i.e. there are
@@ -212,25 +216,75 @@ function load_data(case::String, N::Int64, save_data::Bool=false,
 	ac = (ac_time / (dc_time + ac_time))
 	dc_time = dc * pf_time
 	ac_time = ac * pf_time
+
 	# DC and AC computation time; will be less than the @time macro in main()
 	# since that also has other overhead like network data dict accessing
 	# only write to output.log if we're saving data, i.e. not test runs
 	if save_data == true && log == true
-		runlog = open("$(case)_output_pf.log", "a")
-		println(runlog, "Number of workers: $(nprocs()-1)")
+		runlog = open("$(case)_pf_output_w$(nprocs()-1).log", "a")
+		println(runlog, "Setting up pf time: $(round(setup_time, digits=3)) seconds")
+		println(runlog, "Extracting results time: $(round(reduce_time, digits=3)) seconds")
 		println(runlog, "Total power flow computation time: $(round(pf_time, digits=3)) seconds")
 		println(runlog, "  => dcpf: $(round(dc_time, digits=3)) seconds ($(round(dc*100.0, digits=3))%)")
-		println(runlog, "  => acpf: $(round(ac_time, digits=3)) seconds ($(round(ac*100.0, digits=3))%)")
+		println(runlog, "  => acpf: $(round(ac_time, digits=3)) seconds ($(round(ac*100.0, digits=3))%)"
+					* ". Average time per acpf: $(round(ac_time/N, digits=3)) seconds")
 		println(runlog, "  acpf time is $(round(ac/dc, digits=3)) times longer than dcpf")
-		println(runlog, "Extracting results time: $(round(reduce_time, digits=3)) seconds")
-		println("Total load data performance:")
+		println(runlog, "")  # new line
 		close(runlog)
 	end
+
 	# save data and target to disk
 	if save_data == true
 		save("$(case)_pf_results.jld2", "data", data, "target", target)
 	end
+	println("Total load data performance:")
 	## Uncomment if running in REPL and calling train_net next
 	# return data, target
 	return nothing
 end
+
+"""
+Running on command line (assuming load_data.jl is in current directory):
+`/path/to/julia -p <number of workers> load_data.jl <case name> <number of samples>
+		<force generating new dataset Y/N>`
+"""
+
+function main(args::Array{String})
+	error = false
+	case = args[1]
+	if length(args) != 3
+		println("Incorrect number of arguments provided. Expected 3, received $(length(args))")
+		error = true
+	elseif isfile("$case.m") == false
+		println("$case.m not found in current directory. Exiting...")
+		error = true
+	end
+
+	if error == false
+		N = parse(Int64, args[2])  # number of samples, is Int
+		reload = (args[3]=="Y" || args[3]=="y") ? true : false
+		println("***Program started at $(now())***")  # prints to run_load.log, not case.log
+		# record current time stamp at the start of log; overwrite existing file's content
+		runlog = open("$(case)_pf_output_w$(nprocs()-1).log", "a")
+		println(runlog, now())
+		println(runlog, "Running load_data for $case, $N samples, with $(length(workers())) workers at $(now())")
+		if reload == true
+			println(runlog, "Force generating a new dataset even if one exists.")
+		end
+		close(runlog)
+
+		for i = 1:2   # precompilation run
+			load_data(case, 10)
+			println("Warm up $i successeful at $(now())")
+		end
+		println("Finished warming up at $(now())")
+		println("Starting $case data generation of $N samples with $(length(workers())) workers...")
+		@time load_data(case, N, true, true, reload)  # full set and save outputs to file
+		println("Program finished at $(now()). Exiting...")
+		println("")  # new line
+	end
+end
+
+# ARGS only available if arguments are given on command line; so if runnning in
+# REPL, comment this out, and call `main(["array" "of" "strings"])`
+# main(ARGS)
