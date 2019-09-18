052b5160cc3fd6cf468796cb5bfbb77f2ea5079a
jchen975 <jeffreychen975@gmail.com>
2019-09-18 16:23:38 -0400

    Update init_sol.m

--- pf/init_sol.m
+++ pf/init_sol.m
@@ -2,7 +2,7 @@ function init_sol(str)
     mpopt = mpoption('out.all', 0, 'verbose', 0);
     mpc = runpf(str, mpopt);
     if mpc.iterations ~= 1
-        savecase(['./data/', str], mpc);
+        savecase(['./data/', str], mpc);  % path to matpower case data
         mpc2 = loadcase(str);
         ret = runpf(mpc2, mpopt);
         assert(ret.iterations == 1);
