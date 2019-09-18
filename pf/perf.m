6847fe79264d9e28449bbab7ab5a51b150ca93c5
jchen975 <jeffreychen975@gmail.com>
2019-09-18 19:02:36 -0400

    Prints norm

--- pf/perf.m
+++ pf/perf.m
@@ -28,13 +28,17 @@ function perf(str, mode, T, lambda)
         fprintf(' ACPF with cold start performance results:\n');
         fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_cs_avg, itr_cs_std, itr_max, itr_min);
         fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_avg, et_ac_std, et_ac_max, et_ac_min);
-        fprintf(' DCPF performance results\n');
+        fprintf(' \n DCPF performance results\n');
         fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_avg, et_dc_std, et_dc_max, et_dc_min);
+        fprintf(' \n Norm of (AC - DC)\n');
+        fprintf(' va: %.5f  vm: %.5f\n', norm_va, norm_vm);
     elseif mode == 'hs'
         fprintf(' ACPF with hot start performance results:\n');
         fprintf(' T = %s, lambda = %s\n', T, lambda);
         fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_cs_avg, itr_cs_std, itr_max, itr_min);
         fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_avg, et_ac_std, et_ac_max, et_ac_min);
+        fprintf(' \n Norm of (AC - predicted (hot start))\n');
+        fprintf(' va: %.5f  vm: %.5f\n', norm_va, norm_vm);
     end
     fprintf('\n');
 end
