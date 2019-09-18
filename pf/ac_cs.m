ab23e2108d7b87ea6ae5f14555dd96dcdd41d649
jchen975 <jeffreychen975@gmail.com>
2019-09-18 16:14:04 -0400

    ACPF based on the VM, VA from solved base case

--- pf/ac_cs.m
+++ pf/ac_cs.m
@@ -13,9 +13,7 @@ function [V_M, V_A, itr_et, itr_n, fail] = ac_cs(str, P, Q, DCVM, DCVA)
     fail = [];  % should preallocate for perf, but ret.et is the NR time so I don't care
     
     % run acpf numSample times
-    warm = 0;  % DC result warm start counter
-    flat = 0;  % flat start counter
-    mpopt = mpoption('out.all', 0, 'verbose', 0, 'pf.nr.max_it', 30);
+    mpopt = mpoption('out.all', 0, 'verbose', 1, 'pf.nr.max_it', 30);
     for i = 1:numSample
         if mod(i, 1000) == 0
             fprintf(' >> acpf cold start iteration %d\n', i);
@@ -29,36 +27,11 @@ function [V_M, V_A, itr_et, itr_n, fail] = ac_cs(str, P, Q, DCVM, DCVA)
             itr_n(i) = ret.iterations;
             itr_et(i) = ret.et;
         else
-            % failed ACPF, use DCPF's result as warm start
-            mpc.bus(:, VA) = DCVA(:, i);
-            mpc.bus(:, VM) = DCVM(:, i);
-            ret = runpf(mpc, mpopt);
-            if ret.success == 1 % warm start successful
-                warm = warm + 1;
-                V_M(:, i) = ret.bus(:, VM);
-                V_A(:, i) = ret.bus(:, VA);
-                itr_n(i) = ret.iterations;
-                itr_et(i) = ret.et;
-            else 
-                % try flat start
-                mpc.bus(:, VA) = 0.0; 
-                mpc.bus(:, VM) = 1.0;
-                ret = runpf(mpc, mpopt);
-                if ret.success == 1 % flat start successful
-                    flat = flat + 1;
-                    V_M(:, i) = ret.bus(:, VM);
-                    V_A(:, i) = ret.bus(:, VA);
-                    itr_n(i) = ret.iterations;
-                    itr_et(i) = ret.et;
-                else  % resistance is futile
-                    fail = [fail, i];
-                end
-            end
+            fail = [fail, i];
         end
     end
     if length(fail) > 0
-        fprintf(' Number of successful warm-starts: %i\n', warm);
-        fprintf(' Number of successful flat-starts: %i\n', flat);
+        fprintf(' Number of failed samples: %i\n', length(fail);
     end
     assert(size(P, 2) == numSample && size(Q, 2) == numSample && size(V_M, 2) == numSample && size(V_A, 2) == numSample);
     assert(length(itr_et) == numSample && length(itr_n) == numSample);
