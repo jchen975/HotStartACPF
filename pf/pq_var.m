0e56ab34d16c815b6e0d5c36e30f766f88dc712e
jchen975 <jeffreychen975@gmail.com>
2019-09-18 19:02:54 -0400

    Clean up

--- pf/pq_var.m
+++ pf/pq_var.m
@@ -3,24 +3,16 @@ function [P, Q] = pq_var(str, numSample)
     rng(0);  % set rand seed
     mpc = loadcase(str);
     numBus = size(mpc.bus, 1);
-    non_pq = [];  % indices of PV, slack and isolated buses
-    non_pq_qd = [];  % default QD values at the non PQ buses
     
     %% generate PD variations on all PQ buses, i.e. BUS_TYPE == 1
     P = zeros([numBus, numSample], 'single');  % equivalent of Julia Float32
     for i = 1:numBus
         avg_p = mpc.bus(i, PD);  % bus PD
-        if mpc.bus(i, BUS_TYPE) == 1
-            % avg_p could be negative, but std should be the same as if
-            % it's positive
-            std_p = 5.44130 + 0.17459*sqrt(abs(avg_p)) + 0.001673*abs(avg_p);
-            dist = makedist('Normal', 'mu', avg_p, 'sigma', std_p);
-            P(i, :) = random(dist, 1, numSample);
-        else
-            P(i, :) = mpc.bus(i, PD);  % keep default PD
-            non_pq = [non_pq, i];
-            non_pq_qd = [non_pq_qd; mpc.bus(i, QD)];
-        end
+        % avg_p could be negative, but std should be the same as if
+        % it's positive
+        std_p = 5.44130 + 0.17459*sqrt(abs(avg_p)) + 0.001673*abs(avg_p);
+        dist = makedist('Normal', 'mu', avg_p, 'sigma', std_p);
+        P(i, :) = random(dist, 1, numSample);
     end
     
     %% generate QD variations
@@ -36,7 +28,11 @@ function [P, Q] = pq_var(str, numSample)
     
     % generate QD variations based on P and pf matrices
     Q = P .* tan(acos(pf));
-    Q(non_pq, :) = repmat(non_pq_qd, 1, numSample);  % change back to default qd values
+    
+    % Keep PV buses PD, QD values as is
+    gen_idx = find(mpc.bus(:, BUS_TYPE) == PV);
+    P(gen_idx, :) = repmat(mpc.bus(gen_idx, PD), 1, numSample);
+    Q(gen_idx, :) = repmat(mpc.bus(gen_idx, QD), 1, numSample);
     
     %%
     % save PD, QD to file in the case that one or more columns of P,Q 
