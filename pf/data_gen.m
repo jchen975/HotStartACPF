04835947e3286eab582c769c99d4d9f3ce10622f
jchen975 <jeffreychen975@gmail.com>
2019-09-18 19:02:21 -0400

    Calculate and save voltage phasor diff norm

--- pf/data_gen.m
+++ pf/data_gen.m
@@ -6,11 +6,10 @@ function data_gen()
     %%%%
     cases = ["case30", "case89pegase", "case118", "case145", "case2869pegase"];
     %cases = ["case300", "case1354pegase", "case13659pegase"];
-    
+
     N = 10000;
 
     for c = cases
-        fprintf('\n================ %s ================\n', c);
         %% PF based on PQ variations
         c = char(c);  % weird matlab string format
         init_sol(c);  % solve base case
@@ -38,14 +37,21 @@ function data_gen()
         Q = Q ./ mpc.baseMVA;
         DCVA = deg2rad(DCVA);  % make DCVA, ACVA in radians for num stability
         ACVA = deg2rad(ACVA);
+        
+        % calculate L2 norm between AC and DC voltage phasors
+        norm_va = norm(ACVA - DCVA);
+        norm_vm = norm(ACVM - DCVM);
+        
         data = [DCVA; DCVM; P; Q];
         target = [ACVA; ACVM];
+        
         assert(size(data, 1) == 4*size(P, 1) && size(data, 2) == size(P, 2));
         assert(size(target, 1) == 2*size(P, 1) && size(target, 2) == size(P, 2));
 
-        save(['./results/', c, '_data.mat'], 'data');
-        save(['./results/', c, '_target.mat'], 'target');
-        save(['./results/', c, '_perf_cs.mat'], 'itr_ac', 'et_ac', 'et_dc', 'fail');
+        save(['./results/', c, '_dataset.mat'], 'data', 'target');
+        save(['./results/', c, '_perf_cs.mat'], 'itr_ac', 'et_ac', 'et_dc', 'fail', 'norm_va', 'norm_vm');
+        
+        perf(c, 'cs', '', '');
     end
     fprintf('Data generation finished.\n\n');
 end
