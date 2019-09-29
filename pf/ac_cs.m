d90e94e82631c75df3f80c5cfff727095bd42075
jchen975 <jeffreychen975@gmail.com>
2019-09-29 01:12:51 -0400

    Cold start with flat vm, va

--- pf/ac_cs.m
+++ pf/ac_cs.m
@@ -1,9 +1,15 @@
-function [V_M, V_A, itr_et, itr_n, fail] = ac_cs(str, P, Q, DCVM, DCVA)
+function [V_M, V_A, itr_et, itr_n, fail] = ac_cs(str, P, Q)
     define_constants;
     mpc = loadcase(str);
     
     V_M = zeros(size(P), 'single');  % equivalent of Julia Float32
     V_A = zeros(size(P), 'single');
+    
+    % flat start vm = 1 except PV bus, all va = 0
+    gen_idx = find(mpc.bus(:, BUS_TYPE) == PV);
+    flat_vm = ones(size(V_M, 1), 1, 'single');  
+    flat_vm(gen_idx, :) = mpc.bus(gen_idx, VM);
+    flat_va = zeros(size(flat_vm), 'single');
     
     numSample = size(P, 2);
     itr_n = zeros(numSample, 1);  % number of iteration each sample
@@ -13,13 +19,12 @@ function [V_M, V_A, itr_et, itr_n, fail] = ac_cs(str, P, Q, DCVM, DCVA)
     fail = [];  % should preallocate for perf, but ret.et is the NR time so I don't care
     
     % run acpf numSample times
-    mpopt = mpoption('out.all', 0, 'verbose', 0);  % verbose = 2 if want to print max P,Q mismatch
+    mpopt = mpoption('out.all', 0, 'verbose', 0, 'pf.nr.max_it', 200);  % verbose = 2 if want to print max P,Q mismatch
     for i = 1:numSample
-        if mod(i, 1000) == 0
-            fprintf(' >> acpf cold start iteration %d\n', i);
-        end
         mpc.bus(:, PD) = P(:, i);
         mpc.bus(:, QD) = Q(:, i);
+        mpc.bus(:, VM) = flat_vm;
+        mpc.bus(:, VA) = flat_va;
         ret = runpf(mpc, mpopt);
         if ret.success == 1
             V_M(:, i) = ret.bus(:, VM);
