638ba9440bdbfae872b3d7ba6ba4c9a1baffc25b
jchen975 <jeffreychen975@gmail.com>
2019-10-25 19:31:19 -0400

    Update perf.m

--- pf/perf.m
+++ pf/perf.m
@@ -1,8 +1,8 @@
-function perf(str, mode, T, lambda)
+function perf(case_str, mode, T)
     if mode == 'cold'
-        load(['./results/', str, '_perf_cold.mat']);
+        load(['./results/', case_str, '_perf_cold.mat']);
     else 
-        load(['./results/', str, '_perf_hot_', T, 'T_', lambda, 'lambda.mat']);
+        load(['./results/', case_str, '_perf_hot_', T, 'T.mat']);
     end
     
     et_ac_per_sample = stat(et_ac);
@@ -15,22 +15,19 @@ function perf(str, mode, T, lambda)
     % pass
     et_ac_per_itr = sum(et_ac ./ itr_ac);  
     
-    fprintf('================ %s ================\n', str);
+    fprintf('================ %s ================\n', case_str);
     if mode == 'cold'
         fprintf(' ACPF with cold start performance results:\n');
-        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_per_sample(3), et_ac_per_sample(4));
-        fprintf(' >> elapsed time per itr:    average = %.7f (%i samples in total) \n', et_ac_per_itr, length(et_ac));
+        fprintf(' >> iterations:              avg = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
+        fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_per_sample(3), et_ac_per_sample(4));
+        fprintf(' >> elapsed time per itr:    avg = %.7f (%i samples in total) \n', et_ac_per_itr, length(et_ac));
         fprintf(' \n DCPF performance results\n');
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_per_sample(1), et_dc_per_sample(2), et_dc_per_sample(3), et_dc_per_sample(4));
-
-
+        fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_per_sample(1), et_dc_per_sample(2), et_dc_per_sample(3), et_dc_per_sample(4));
     elseif mode == 'hot'
         fprintf(' ACPF with hot start performance results:\n');
-        fprintf(' T = %s, lambda = %s\n', T, lambda);
-        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_sample(3), et_ac_sample(4));
-
+        fprintf(' T = %s\n', T);
+        fprintf(' >> iterations:              avg = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
+        fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_sample(3), et_ac_sample(4));
     end
     fprintf('\n');
 end
