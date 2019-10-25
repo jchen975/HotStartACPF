0f5c67518182b70aceed5b0cd8504b22a6738565
jchen975 <jeffreychen975@gmail.com>
2019-10-25 18:21:08 -0400

    Update perf.m

--- pf/perf.m
+++ pf/perf.m
@@ -1,44 +1,40 @@
 function perf(str, mode, T, lambda)
-    if mode == 'cs'
-        load(['./results/', str, '_perf_cs.mat']);
+    if mode == 'cold'
+        load(['./results/', str, '_perf_cold.mat']);
     else 
-        load(['./results/', str, '_perf_hs_', T, 'T_', lambda, 'lambda.mat']);
+        load(['./results/', str, '_perf_hot_', T, 'T_', lambda, 'lambda.mat']);
     end
-    et_ac_avg = mean(et_ac);
-    et_ac_std = std(et_ac);
     
-    itr_cs_avg = mean(itr_ac);
-    itr_cs_std = std(itr_ac);
-    
-    et_ac_max = max(et_ac);
-    et_ac_min = min(et_ac);
-    
-    itr_max = max(itr_ac);
-    itr_min = min(itr_ac);
-    
-    if mode == 'cs'  % hot-start won't have dc fields
-        et_dc_avg = mean(et_dc);
-        et_dc_std = std(et_dc);
-        et_dc_max = max(et_dc);
-        et_dc_min = min(et_dc);
+    et_ac_per_sample = stat(et_ac);
+    itr_per_sample = stat(itr_ac);
+    if mode == 'cold'  % hot-start won't have dc fields
+        et_dc_per_sample = stat(et_dc);
     end
+    
+    % average 1 iteration time for all N samples, compare this with forward
+    % pass
+    et_ac_per_itr = sum(et_ac ./ itr_ac);  
     
     fprintf('================ %s ================\n', str);
-    if mode == 'cs'
+    if mode == 'cold'
         fprintf(' ACPF with cold start performance results:\n');
-        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_cs_avg, itr_cs_std, itr_max, itr_min);
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_avg, et_ac_std, et_ac_max, et_ac_min);
+        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
+        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_per_sample(3), et_ac_per_sample(4));
+        fprintf(' >> elapsed time per itr:    average = %.7f (%i samples in total) \n', et_ac_per_itr, length(et_ac));
         fprintf(' \n DCPF performance results\n');
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_avg, et_dc_std, et_dc_max, et_dc_min);
-        fprintf(' \n Norm of (AC - DC)\n');
-        fprintf(' va: %.5f  vm: %.5f\n', norm_va, norm_vm);
-    elseif mode == 'hs'
+        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_per_sample(1), et_dc_per_sample(2), et_dc_per_sample(3), et_dc_per_sample(4));
+
+
+    elseif mode == 'hot'
         fprintf(' ACPF with hot start performance results:\n');
         fprintf(' T = %s, lambda = %s\n', T, lambda);
-        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_cs_avg, itr_cs_std, itr_max, itr_min);
-        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_avg, et_ac_std, et_ac_max, et_ac_min);
-        fprintf(' \n Norm of (AC - predicted (hot start))\n');
-        fprintf(' va: %.5f  vm: %.5f\n', norm_va, norm_vm);
+        fprintf(' >> iterations:              average = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
+        fprintf(' >> elapsed time per sample: average = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_sample(3), et_ac_sample(4));
+
     end
     fprintf('\n');
+end
+
+function ret = stat(arr)
+    ret = [mean(arr), std(arr), max(arr), min(arr)];
 end
