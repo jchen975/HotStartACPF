97d97aeaa5a8dddda64b98d0caa117b805cc184f
jchen975 <jeffreychen975@gmail.com>
2020-01-28 23:45:16 -0500

    Update perf.m

--- pf/perf.m
+++ pf/perf.m
@@ -1,8 +1,12 @@
 function perf(case_str, mode, T)
+    dir = 'D:/Work/Research/DC-Power-Flow-Correction/pf';
+%     dir = 'D:/Work/Research/Cluster Results/';
     if strcmp(mode, 'cold')
-        load(['./results/', case_str, '_perf_cold.mat']);
+        load([dir, '/results/', case_str, '_perf_cold.mat']);
+%         load([dir, case_str, '/', case_str, '_perf_cold.mat']);
     else 
-        load(['./results/', case_str, '_perf_hot_T=', T, '.mat']);
+        load([dir, '/results/', case_str, '_perf_hot_T=', T, '.mat']);
+%         load([dir, case_str, '/', case_str, '_perf_hot_T=', T, '.mat']);
     end
     
     et_ac_per_sample = stat(et_ac);
@@ -20,7 +24,7 @@ function perf(case_str, mode, T)
 %         fprintf(' >> elapsed time per itr:    avg = %.7f (%i samples in total) \n', et_ac_per_itr, length(et_ac));
         fprintf(' \n DCPF performance results\n');
         fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_per_sample(1), et_dc_per_sample(2), et_dc_per_sample(3), et_dc_per_sample(4));
-        fprintf(' >> total elapsed time: %.7f\n', sum(et_dc));
+        fprintf(' >> total elapsed time:            %.7f\n', sum(et_dc));
     elseif strcmp(mode, 'hot')
         fprintf(' ACPF with hot start performance results:\n');
         fprintf(' T = %s\n', T);
