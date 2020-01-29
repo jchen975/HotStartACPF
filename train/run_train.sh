b1fb33c42c0551880083c17eb1545380fe89ed26
jchen975 <jeffreychen975@gmail.com>
2020-01-28 23:46:18 -0500

    Update run_train.sh

--- train/run_train.sh
+++ train/run_train.sh
@@ -17,29 +17,28 @@ module load nixpkgs/16.09 gcc/7.3.0 cuda/10.0.130 cudnn/7.6 julia/1.3.0 # enable
 export LD_LIBRARY_PATH="${EBROOTCUDA}/lib64:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${EBROOTCUDNN}/lib64:${LD_LIBRARY_PATH}"
 
-casefiles=("case30" "case118");
-T=("0.1" "0.15" "0.2" "0.25" "0.3");
-for c in ${casefiles[*]}
-do
-    for t in ${T[*]}
-    do
-        echo ">> T = $t"  >> run_train.log 2>&1
-        julia train.jl "$c" "$t" "conv" "2" "retrain" "" >> run_train.log 2>&1
-    done
-done
-
-
-T2=("0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8")
-for t in ${T2[*]}
+case="case118";
+T1=("2000" "3000" "4000" "5000" "6000");
+for t in ${T1[*]}
 do
     echo ">> T = $t"  >> run_train.log 2>&1
-    julia train.jl "case2869pegase" "$t" "conv" "2" "retrain" "" >> run_train.log 2>&1
+    julia train.jl "$case" "$t" "retrain" "" >> run_train.log 2>&1
 done
 
+case="case2869pegase";
+T2=("3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000" "11000" "12000");
 for t in ${T2[*]}
 do
+   echo ">> T = $t"  >> run_train.log 2>&1
+   julia train.jl "$case" "$t" "retrain" "" >> run_train.log 2>&1
+done
+
+case="case300";
+T3=("2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000");
+for t in ${T3[*]}
+do
     echo ">> T = $t"  >> run_train.log 2>&1
-    julia train.jl "case300" "$t" "conv" "2" "retrain" "failmode" >> run_train.log 2>&1
+    julia train.jl "$case" "$t" "retrain" "failmode" >> run_train.log 2>&1
 done
 
 
