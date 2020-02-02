174c3eec2e4a802f7eaff28d22fe3244e2eb9c9e
jchen975 <jeffreychen975@gmail.com>
2020-02-01 21:45:58 -0500

    Updates

--- pf/ac_hot.m
+++ pf/ac_hot.m
@@ -1,10 +1,10 @@
-% c and T are both strings, ex. ac_hot('case30', '0.2')
+% c and T are both strings, ex. ac_hot('case30', '2000')
 function ac_hot(c, T)  
     define_constants;
     c = char(c);
     mpc = loadcase(c);
-    dir_predict = 'D:/Work/Research/Cluster Results/';
-    dir_data = 'D:/Work/Research/DC-Power-Flow-Correction/results/';
+    dir_predict = 'D:/Work/Research/Cluster Results/Jan 2020/';
+    dir_data = 'D:/Work/Research/DC-Power-Flow-Correction/pf/results/';
     fpredict = [dir_predict, c, '/', c, '_predict_T=', T, '.mat'];
     fdata = [dir_data, c, '_dataset.mat'];
     
@@ -14,22 +14,22 @@ function ac_hot(c, T)
         load(fdata);
 
         N = size(P, 2);
-        numSample = int32((1-T)*N);  % N \ T
+%         numSample = int32((1-T)*N);  % N \ T
 
-        P = P(:, (N-numSample+1):end);
-        Q = Q(:, (N-numSample+1):end);
+        P = P(:, (T+1):end);
+        Q = Q(:, (T+1):end);
       
         max_itr = 10;
-        itr_ac = zeros(numSample, 1);  % number of iteration each sample
-        et_ac = zeros(numSample, 1);  % iteration elapsed time
-        mismatch_hot = zeros([max_itr, numSample], 'single');  % PQ mismatch info
+        itr_ac = zeros(N-T, 1);  % number of iteration each sample
+        et_ac = zeros(N-T, 1);  % iteration elapsed time
+        mismatch_hot = zeros([max_itr, N-T], 'single');  % PQ mismatch info
 
         % arr for i-th failed NR
         fail = [];  % should preallocate for perf, but ret.et is the NR time so I don't care
 
         % run acpf numSample times
         mpopt = mpoption('out.all', 0, 'verbose', 0, 'pf.tol', 1e-3);
-        for i = 1:numSample
+        for i = 1:(N-T)
             mpc.bus(:, PD) = P(:, i);
             mpc.bus(:, QD) = Q(:, i);
             mpc.bus(:, VM) = V_M(:, i); 
@@ -41,14 +41,15 @@ function ac_hot(c, T)
                 et_ac(i) = ret.et;
                 mismatch_hot(:, i) = ret.mismatch;
             else 
+                itr_ac(i) = max_itr;
                 fail = [fail, i];
             end
         end
         if length(fail) > 0
             fprintf(' Number of failed ACPF: %i\n', length(fail));
         end
-        assert(size(P, 2) == numSample && size(Q, 2) == numSample && size(V_M, 2) == numSample && size(V_A, 2) == numSample);
-        assert(length(et_ac) == numSample && length(itr_ac) == numSample);
+%         assert(size(P, 2) == numSample && size(Q, 2) == numSample && size(V_M, 2) == numSample && size(V_A, 2) == numSample);
+%         assert(length(et_ac) == numSample && length(itr_ac) == numSample);
         
         T_str = num2str(T);
         fn = [dir_predict, c, '/', c, '_perf_hot_T=', T_str, '.mat'];
--- pf/perf.m
+++ pf/perf.m
@@ -1,12 +1,10 @@
 function perf(case_str, mode, T)
-    dir = 'D:/Work/Research/DC-Power-Flow-Correction/pf';
-%     dir = 'D:/Work/Research/Cluster Results/';
     if strcmp(mode, 'cold')
+        dir = 'D:/Work/Research/DC-Power-Flow-Correction/pf';
         load([dir, '/results/', case_str, '_perf_cold.mat']);
-%         load([dir, case_str, '/', case_str, '_perf_cold.mat']);
     else 
-        load([dir, '/results/', case_str, '_perf_hot_T=', T, '.mat']);
-%         load([dir, case_str, '/', case_str, '_perf_hot_T=', T, '.mat']);
+        dir = 'D:/Work/Research/Cluster Results/Jan 2020/';
+        load([dir, case_str, '/', case_str, '_perf_hot_T=', T, '.mat']);
     end
     
     et_ac_per_sample = stat(et_ac);
@@ -21,7 +19,6 @@ function perf(case_str, mode, T)
         fprintf(' >> iterations:              avg = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
         fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_per_sample(3), et_ac_per_sample(4));
         fprintf(' >> total elapsed time:            %.7f\n', sum(et_ac));
-%         fprintf(' >> elapsed time per itr:    avg = %.7f (%i samples in total) \n', et_ac_per_itr, length(et_ac));
         fprintf(' \n DCPF performance results\n');
         fprintf(' >> elapsed time per sample: avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_dc_per_sample(1), et_dc_per_sample(2), et_dc_per_sample(3), et_dc_per_sample(4));
         fprintf(' >> total elapsed time:            %.7f\n', sum(et_dc));
@@ -30,8 +27,6 @@ function perf(case_str, mode, T)
         fprintf(' T = %s\n', T);
         fprintf(' >> iterations:                avg = %.3f, std = %.3f, max = %i, min = %i\n', itr_per_sample(1), itr_per_sample(2), itr_per_sample(3), itr_per_sample(4));
         fprintf(' >> elapsed time per sample:   avg = %.7f, std = %.7f, max = %.7f, min = %.7f\n', et_ac_per_sample(1), et_ac_per_sample(2), et_ac_per_sample(3), et_ac_per_sample(4));
-        fprintf(' >> N\\T total elapsed time:          %.7f\n', sum(et_ac));
-        fprintf(' >> extrapolated total elapsed time: %.7f\n', (sum(et_ac))/(1-str2num(T)));
     end
     fprintf('\n');
 end
